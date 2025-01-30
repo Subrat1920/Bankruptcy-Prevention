c25ae47d5bf731b039db1716d0a346deaf45324f
Subuking001 <3subratmishra1sep@gmail.com>
2025-01-30 18:38:10 +0530

    updates in project setup

---
+++ src/components/constants/training_pipline/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+
+'''
+DATA INGESTION RELATED CONSTANT START WITH DATA_INGESTION VAR NAME
+'''
+
+DATA_INGESTION_COLLECTION_NAME:str='Bankruptcy Prevention'
+DATA_INGESTION_DATABASE_NAME:str='pre_processed_data.csv'
+DATA_INGESTION_DIR_NAME:str=''
