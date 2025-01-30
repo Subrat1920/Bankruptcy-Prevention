d8f39125487eedca86dd5f0e0846fc724c7401b6
Subuking001 <3subratmishra1sep@gmail.com>
2025-01-30 11:46:18 +0530

    setup.py ready

--- setup.py
+++ setup.py
@@ -3,19 +3,28 @@ from typing import List
 
 HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path)->List[str]:
-    requirements = []
-    with open(file_path, 'r') as file:
-        requirements = file.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
-
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-    return requirements
+def get_requirements()->List[str]:
+    ## This function will return the list of requirements
+    requirement_list:List[str]=[]
+    try:
+        with open('requirements.txt', 'r') as file:
+            ## read lines from requirements.txt
+            lines = file.readlines()
+            ## process each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines ans -e.
+                if requirement and not requirement.startswith(HYPHEN_E_DOT):
+                    requirement_list.append(requirement)
+    except FileNotFoundError:
+        print('Requirements.txt not found!!!')
+    
+    return requirement_list
+                    
 
 setup(
     name = 'Bankruptcy Prevention',
     version = '0.0.1',
     packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    install_requires = get_requirements()
 )
