61e83d08049299f09b8859977da3b58d311057e2
Subuking001 <3subratmishra1sep@gmail.com>
2025-01-31 12:53:47 +0530

    project setup updates

--- src/components/constants/training_pipline/__init__.py
+++ src/components/constants/training_pipline/__init__.py
@@ -5,9 +5,21 @@ import pandas as pd
 
 
 '''
+Define common constant variable for training pipelin
+'''
+TARGET_COLUMN = "class"
+PIPLINE_NAME = "Bankruptcy Security"
+ARTIFACT_DIR = "Artifacts"
+FILE_NAME = "pre_processed_data.csv"
+
+
+'''
 DATA INGESTION RELATED CONSTANT START WITH DATA_INGESTION VAR NAME
 '''
 
-DATA_INGESTION_COLLECTION_NAME:str='Bankruptcy Prevention'
-DATA_INGESTION_DATABASE_NAME:str='pre_processed_data.csv'
-DATA_INGESTION_DIR_NAME:str=''
+DATA_INGESTION_COLLECTION_NAME:str='BankruptcyPrevention'
+DATA_INGESTION_DATABASE_NAME:str='BankruptcyPreventionData'
+DATA_INGESTION_DIR_NAME:str='data_ingestion'
+DATA_INGESTION_FEATURE_STORE_DIR:str='feature_store'
+DATA_INGESTION_INGESTED_DIR:str='ingested'
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=0.2
